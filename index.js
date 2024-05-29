cdf12e5c11c735d5ed15e7219a9f0ee93120eba8
Emmanuel24th <emmanueljoseph168@gmail.com>
2024-05-29 22:38:25 +0100

    eddited app.get line

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000; // Corrected quotes around 3000
 app.get('/', (req, res) => { // Corrected quotes around '/'
-    res.send('We arefinished with this tutorial!'); // Corrected quotes around the message
+    res.send('We are finished with this tutorial!'); // Corrected quotes around the message
 });
 app.listen(port, () => {
     console.log(`Listening at http://localhost:${port}`);
